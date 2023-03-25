b0e6aa124b19f1f9a749e6b5a97c1ceee213f126
m.pruglo <michael.pruglo.work@gmail.com>
2023-03-25 22:51:40 +0200

    factory

---
+++ factory.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+
+from abc import ABC, abstractmethod
+import random
+
+class Transport(ABC):
+    @abstractmethod
+    def deliver(self): pass
+
+class Truck(Transport):
+    def deliver(self):
+        print("Deliver by land in a box")
+
+class Ship(Transport):
+    def deliver(self):
+        print("Deliver by sea in a container")
+
+
+class Logistics(ABC):
+    def plan_delivery(self):
+        t = self.create_transport()
+        t.deliver()
+
+    @abstractmethod
+    def create_transport(self) -> Transport:
+        pass
+
+class RoadLogistics(Logistics):
+    def create_transport(self):
+        return Truck()
+
+class SeaLogistics(Logistics):
+    def create_transport(self):
+        return Ship()
+
+
+if __name__ == "__main__":
+    log = RoadLogistics() if random.randint(0,1) else SeaLogistics()
+    log.plan_delivery()
